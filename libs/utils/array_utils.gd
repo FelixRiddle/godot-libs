1d15d157fbdf29b55224b423d2efaaf5f5c4c074
Felix <fedecraft3312@gmail.com>
2022-05-10 14:58:27 -0300

    Added functions for arrays

--- libs/utils/array_utils.gd
+++ libs/utils/array_utils.gd
@@ -2,9 +2,6 @@ class_name ArrayUtils
 
 # Counting Sort ###
 static func counting_sort(input:Array) -> Array:
-	if(typeof(input) != TYPE_ARRAY):
-		return input
-	
 	# k is the maximum value of the non-negative key values
 	var k = get_max_number_in_array(input)
 	var count = create_zero_array(k + 1)
@@ -26,24 +23,22 @@ static func counting_sort(input:Array) -> Array:
 
 # Creates an array of zeros
 static func create_zero_array(length:int) -> Array:
-	if(typeof(length) != TYPE_INT):
-		return []
-	
 	var temp = []
 	for _i in range(length):
 		temp.append(0)
 	return temp
 
 
-# TODO:
 # Get an array of the last items
-#static func get_last_items(arr:Array, length:int):
+static func get_last_items(arr:Array, length:int) -> Array:
+	var new_arr = []
+	for i in range(length):
+		new_arr.push_back(arr[(arr.size() - 1) - i])
+	return new_arr
+
 
 # Get the max number in an array
 static func get_max_number_in_array(arr:Array) -> int:
-	if(typeof(arr) != TYPE_ARRAY):
-		return 0
-	
 	var curr = arr[0]
 	for i in range(arr.size()):
 		curr = max(curr, arr[i])
@@ -64,6 +59,14 @@ static func increase_length(arr:Array, length:int) -> Array:
 # Alias for increase_length
 static func increase_size(arr:Array, length:int) -> Array:
 	return increase_length(arr, length)
+
+
+# Reverse an array
+static func reverse_array(arr:Array) -> Array:
+	var new_arr = []
+	for i in range(arr.size()):
+		new_arr.push_back(arr[(arr.size() - 1) - i])
+	return new_arr
 
 
 # Shrink array
